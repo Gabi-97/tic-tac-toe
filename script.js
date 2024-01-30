fd220288336f4618b3777601a2d336f49543324c
Feyru <cakeesky@gmail.com>
2024-01-30 15:16:29 +0100

    added win/gameover conditions

--- script.js
+++ script.js
@@ -10,7 +10,6 @@ const Gameboard = (function() {
     };
 })();
 
-
 //objects for the players 
 const Players =  {
     playerOne: {
@@ -57,26 +56,72 @@ const GameFlow = {
             }
         } else alert("Please enter the value between 1 and 3");
     },
+    isGameBoardFull() {
+        return Gameboard.gameboard.every(cell => cell !== "");
+    },
+    checkWinCondition() {
+        //check rows 
+        for(let i = 0; i < 3; i++) {
+            if ( 
+                Gameboard.gameboard[i * 3] === Gameboard.gameboard[i * 3 + 1] &&
+                Gameboard.gameboard[i * 3] === Gameboard.gameboard[i * 3 + 2] &&
+                Gameboard.gameboard[i * 3] !== ""
+            ) {
+                return true;
+            }
+        }
+        //check columns
+        for(let i = 0; i < 3; i++) {
+            if (
+                Gameboard.gameboard[i] === Gameboard.gameboard[i + 3] &&
+                Gameboard.gameboard[i] === Gameboard.gameboard[i + 6] &&
+                Gameboard.gameboard[i] !== ""
+            ) {
+                return true;
+            }
+        }
+        //check diagonal
+        if(
+            (Gameboard.gameboard[0] === Gameboard.gameboard[4] &&
+             Gameboard.gameboard[0] === Gameboard.gameboard[8] &&
+             Gameboard.gameboard[0] !== "") ||
+            (Gameboard.gameboard[2] === Gameboard.gameboard[4] &&
+             Gameboard.gameboard[2] === Gameboard.gameboard[6] &&
+             Gameboard.gameboard[2] !== "")
+        ) {
+            return true;
+        }
+        return false;
+    },
     //main game loop
     playGame() { 
         currPlayer = "playerOne";
+        
+        while(!this.isGameBoardFull()) {
+        
+        //get the input from the current player
+        const userInput = this.getPlayerInput();
 
-        for(let i = 0; i <= 9; i++) {
-            //display the gameboard
+        //fill the board based on players input
+        this.playerMove(Players[currPlayer], userInput.row, userInput.column);
+        
+        //display winner or switch to the next player
+        if(this.checkWinCondition()) {
             this.displayGameBoard();
-
-            //get the input from the current player
-            const userInput = this.getPlayerInput();
-
-            //fill the board based on players input
-            this.playerMove(Players[currPlayer], userInput.row, userInput.column);
-
+            console.log(Players[currPlayer].name + " is the winner");
+            //break out of the loop if there is a winner
+            break;
+        }
+        else {
             //switch players
             currPlayer = this.switchPlayer(currPlayer);
-    }
-}    
+            }
+        this.displayGameBoard();
+    } 
+    }    
 }
 GameFlow.playGame();
+
 
 
 
